479ce845f15a4cd3f8e6ced4750d8529d48e280e
ArvinJoy <arvinpangalo@g.cjc.edu.ph>
2025-09-04 11:51:48 +0800

    Constant Map Style

--- app/(tabs)/commuter/index.tsx
+++ app/(tabs)/commuter/index.tsx
@@ -1,3 +1,4 @@
+import { mapDarkStyle } from "@/assets/data/mapDarkStyle";
 import { BottomSheet } from "@/components/commuter/BottomSheet";
 import { UserMarker3D } from "@/components/model/UserMarker3D";
 import { useAuth } from "@/contexts/AuthContext";
@@ -375,7 +376,7 @@ export function CommuterHomeScreen() {
           ref={mapRef}
           style={StyleSheet.absoluteFill}
           provider="google"
-          customMapStyle={theme === "dark" ? mapDarkStyle : []}
+          customMapStyle={theme === "dark" ? [...mapDarkStyle] : []}
           initialRegion={{
             latitude: 6.7536,
             longitude: 125.356,
@@ -866,86 +867,5 @@ const styles = StyleSheet.create({
     marginTop: -40, // Adjust this to have the tip of the pin at the center
   },
 });
-
-const mapDarkStyle = [
-  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-  {
-    featureType: "administrative.locality",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }],
-  },
-  {
-    featureType: "poi",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [{ color: "#263c3f" }],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#6b9a76" }],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [{ color: "#38414e" }],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.stroke",
-    stylers: [{ color: "#212a37" }],
-  },
-  {
-    featureType: "road",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#9ca5b3" }],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [{ color: "#746855" }],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "geometry.stroke",
-    stylers: [{ color: "#1f2835" }],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#f3d19c" }],
-  },
-  {
-    featureType: "transit",
-    elementType: "geometry",
-    stylers: [{ color: "#2f3948" }],
-  },
-  {
-    featureType: "transit.station",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#d59563" }],
-  },
-  {
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [{ color: "#17263c" }],
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#515c6d" }],
-  },
-  {
-    featureType: "water",
-    elementType: "labels.text.stroke",
-    stylers: [{ color: "#17263c" }],
-  },
-];
 
 export default CommuterHomeScreen;
